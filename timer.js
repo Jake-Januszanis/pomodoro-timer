1051878c6c3e112248c10ed690bd0d71755e886e
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-02-08 14:46:34 -0700

    Updated design//Added function to keep track of time studying

--- timer.js
+++ timer.js
@@ -1,16 +1,27 @@
 
 
 let countdown;
-const reset = document.getElementById("clear");
+const reset = document.getElementById("reset-btn");
 const display = document.querySelector(".timer")
 const buttons = document.querySelectorAll(".buttons");
 const totalCount = document.querySelector(".total-count");
+const title = document.querySelector('title');
+const body = document.querySelector('body');
+const totalCountDisplay = document.querySelector(".total-count-display");
+const totalCountBtn = document.querySelector(".total-count-display-btn");
 let count = 0;
  
 
 buttons.forEach((button) => {
     button.addEventListener('click', function() {
         button.name === 'work' ? workTimer(button.value) : breakTimer(button.value);
+        if (button.value === '300') {
+            document.body.style.backgroundColor = "#40916c";
+        } else if (button.value === '600') {
+            document.body.style.backgroundColor = "#f9c74f";
+        } else {
+            document.body.style.backgroundColor = "#93c9ff";
+        }
     })
 })
 
@@ -21,7 +32,22 @@ reset.addEventListener("click", function() {
 
 function updateCount() {
     count++;
-    totalCount.innerHTML = count;
+    totalCount.innerText = ""
+    for (i = 0; i < count; i++){
+        totalCount.innerText += "*"
+    }
+}
+
+totalCountBtn.addEventListener('click', function() {
+    let time = count * 25;
+    let hours = Math.floor(time / 60);
+    let minutes = time % 60;
+    totalCountDisplay.innerText = `Total time spent studying today is ${hours} hours and ${minutes} minutes`;
+});
+
+function displayTotalTime() {
+    let time = count * 25;
+    totalCountDisplay.innerText = time;
 }
 
 function workTimer(seconds) {
@@ -67,6 +93,7 @@ function timerDisplay(seconds) {
     const sec = Math.floor(seconds % 60);
     const properSeconds = sec < 10 ? `0${sec}`: sec;
     display.innerHTML = `${minutes}:${properSeconds}`;
+    title.innerText = `Pomo Timer - ${minutes}:${properSeconds}`
 }
 
 
